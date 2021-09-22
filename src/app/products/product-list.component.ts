030c25ce43060475d09a914e2545df4cf25f992a
Jacinto Mba <jcantero@fexco.com>
2021-09-22 11:04:41 +0100

    Interpolation for product table

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,21 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  products: any[] = [{
+    id: 2,
+    name: 'Garden Cart',
+    code: 'GDN-0023',
+    releaseDate: 'March 18, 2021',
+    price: 32.99,
+    rating: 4.2,
+    image: 'assets/images/garden_cart.png'
+  }, {
+    id: 5,
+    name: 'Hammer',
+    code: 'TBX-0048',
+    releaseDate: 'March 21, 2021',
+    price: 8.9,
+    rating: 4.8,
+    image: 'assets/images/hammer.png'
+  }];
 }
