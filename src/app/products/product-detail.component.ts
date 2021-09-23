1ce2bbfcf9ad7cfd1b65111b48b7aacf3ce8250d
Jacinto Mba Cantero <jacmba@gmail.com>
2021-09-23 17:23:26 +0100

    Advanced routing and guards

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,12 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string;
+  product: IProduct | undefined;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router) {
     this.pageTitle = 'Product Detail';
   }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.params.id);
+    this.productService.getProducts().subscribe(
+      ps => {
+        const p = ps.filter(x => x.id === id);
+        this.product = p[0];
+        console.log(JSON.stringify(this.product, null, 2));
+
+        if(!this.product) {
+          this.router.navigate([`product-${id}-not-found`]);
+        }
+      }
+    );
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
