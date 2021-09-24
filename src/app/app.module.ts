4f6ea7488d39a64d04862015ebd90301f5ef0a8b
Jacinto Mba Cantero <jacmba@gmail.com>
2021-09-24 13:07:59 +0100

    Modularity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
-import { EurifyPipe } from './shared/eurify.pipe';
-import { StarComponent } from './shared/star.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './shared/welcome.component';
 import { NotFoundComponent } from './shared/not-found.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacePipe,
-    EurifyPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent,
     NotFoundComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
-    {path: 'products', component: ProductListComponent},
-    {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]},
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot([
     {path: 'welcome', component: WelcomeComponent},
     {path: '', redirectTo: 'welcome', pathMatch: 'full'},
     {path: '**', component: NotFoundComponent}
-  ])],
+  ]), ProductModule],
   providers: [],
   bootstrap: [AppComponent],
 })
