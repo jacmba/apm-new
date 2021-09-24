13acb989a82add8b47e680bd9632d2b55cce9731
Jacinto Mba <jcantero@fexco.com>
2021-09-24 13:07:59 +0100

    Modularity

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacePipe } from '../shared/convert-to-space.pipe';
+import { EurifyPipe } from '../shared/eurify.pipe';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacePipe,
+    EurifyPipe
+  ],
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent},
+    {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]}
+    ])
+  ]
+})
+export class ProductModule { }
